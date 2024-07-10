22dcac41fbbf0c2c568e0985b83c225b33eb4e66
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-07-11 01:30:01 +0300

    dd

--- app.js
+++ app.js
@@ -1,48 +1,64 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { Octokit, App } from 'octokit';
+import { createAppAuth } from "@octokit/auth-app";
+
+const owner = 'Artanty'
+const repo = 'shared-secrets'
+const secret_name = 'WIRE_URL'
 
 dotenv.config();
 
 const appId = process.env.APP_ID;
-const privateKey = process.env.PRIVATE_KEY;
+const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
 const secret = process.env.WEBHOOK_SECRET;
-const enterpriseHostname = process.env.ENTERPRISE_HOSTNAME;
 
-const githubApp = new App({
-  appId,
-  privateKey,
-  webhooks: {
-    secret
-  },
-  ...(enterpriseHostname && {
-    Octokit: Octokit.defaults({
-      baseUrl: `https://${enterpriseHostname}/api/v3`
-    })
-  })
-});
-
-const { data } = await githubApp.octokit.request('/app');
-githubApp.octokit.log.debug(`Authenticated as '${data.name}'`);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/webhook', async (req, res) => {
-  try {
-    const payload = req.body;
-    if (payload.action === 'workflow_dispatch') {
-      const secretUrl = process.env.SECRET_URL;
-      res.status(200).send({ secretUrl });
-    } else {
-      res.status(200).send({ message: 'No action required' });
-    }
-  } catch (error) {
-    res.status(500).send({ error: 'Internal Server Error' });
-  }
+// console.log(privateKey)
+// const githubApp = new App({
+//   appId,
+//   privateKey
+// });
+
+// const { data } = await githubApp.octokit.request('/app');
+// githubApp.octokit.log.debug(`Authenticated as '${data.name}'`);
+
+// console.log(`data.name: ${data}`)
+
+
+// Object.keys(data).forEach(el =>{
+//   console.log(el)
+//   console.log(data[el])
+// })
+// await githubApp.octokit.rest.issues.create({
+//   owner: owner,
+//   repo: repo,
+//   title: "Hello, world!",
+//   body: "I created this issue using Octokit!",
+//   installationId: 940632
+// });
+
+const app2 = new App({ appId, privateKey });
+// const { data: slug } = await app2.octokit.rest.apps.getAuthenticated();
+const octokit = await app2.getInstallationOctokit(52641163);
+// await octokit.rest.issues.create({
+//   owner: owner,
+//   repo: repo,
+//   title: "Hello world from " + slug,
+// });
+
+const { data } = await octokit.rest.actions.getRepoSecret({
+  owner,
+  repo,
+  secret_name,
 });
+console.log(data)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
