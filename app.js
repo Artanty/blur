f7003539688e2c9596c44ee7e288da9307dc4cc0
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-07-10 10:21:03 +0300

    try 1 path;

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ dotenv.config()
 
 // Set configured values
 const appId = process.env.APP_ID
-const privateKeyPath = process.env.PRIVATE_KEY_PATH
-const privateKey = fs.readFileSync(privateKeyPath, 'utf8')
+// const privateKeyPath = process.env.PRIVATE_KEY_PATH
+// const privateKey = fs.readFileSync(privateKeyPath, 'utf8')
+const privateKey = process.env.PRIVATE_KEY_PATH
 const secret = process.env.WEBHOOK_SECRET
 const enterpriseHostname = process.env.ENTERPRISE_HOSTNAME
 const messageForNewPRs = fs.readFileSync('./message.md', 'utf8')
